995566da5bad9c33003853106a0cc4ad8a1c52c6
Bartek <bartosz.kociecki@gmail.com>
2023-10-01 11:56:27 +0200

    js

---
+++ JS-udemy/script.js
@@ -0,0 +1,6 @@
+console.log("Hello World!");
+document.write(Math.E);
+var number = Math.random();
+document.write(" lol \n"+number);
+var elem = document.getElementById("onas");
+elem.innerHTML = "O Was";
